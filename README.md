0eacd403a26174bd3104981f4b1c35488314d7f6
ahmadraza365 <ahmadrazabhatti30@gmail.com>
2023-07-22 22:06:02 +0500

    Following Tutorial

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ yarn
 yarn dev
 ```
 
+# Run Following CMD to Create Visemes from ogg file
+
+./rhubarb -f json ..\3D-Character-Lip-Sync\public\audio\thirstyCrow.ogg -o thirstyCrow.json
+
+./rhubarb -f json [ogg file path] -o [output file name.json]
 
 ![image](https://user-images.githubusercontent.com/6551176/221732091-23ee52cb-4150-42fa-b998-43628d7a6b0d.png)
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,21 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Character } from "./Character";
+import { useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 
 export const Experience = () => {
+  const backgroundTexture = useTexture("/textures/roomBackground.jpg");
+  const backgroundTexture2 = useTexture("/textures/roomBackground2.jpg");
+  const viewPort = useThree((state) => state.viewport);
+
   return (
     <>
       <OrbitControls />
+      <Character position={[0, -3, 4]} scale={2} />
+      <Environment preset="sunset" />
       <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
+        <planeGeometry args={[viewPort.width, viewPort.height]} />
+        <meshBasicMaterial map={backgroundTexture2} />
       </mesh>
     </>
   );
